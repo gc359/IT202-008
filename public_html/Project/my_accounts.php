083cdca464fc07d4dbe16779d0ed69d4447b8e3a
G Cato <gc359@njit.edu>
2023-05-04 20:56:44 -0400

    done with my account display

--- public_html/Project/my_accounts.php
+++ public_html/Project/my_accounts.php
@@ -1,41 +1,59 @@
 <?php
-require(__DIR__ . "/../../partials/nav.php");
+require_once(__DIR__ . "/../../partials/nav.php");
+is_logged_in(true);
 ?>
+
+
+<?php
+
+$user_id = get_user_id();
+$db = getDB();
+// Query the database for the user's accounts
+$stmt = $db->prepare('SELECT account_number, account_type, modified, balance FROM Accounts WHERE user_id = ? ORDER BY modified DESC LIMIT 5');
+$stmt->execute([$user_id]);
+$accounts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Accounts</title>
+</head>
+<body>
 
 <h1>My Accounts</h1>
-<?php
-// Assume $user_id is the ID of the current user
-list_accounts($user_id);
-?>
 
+<?php if (count($accounts) > 0) : ?>
+    <table>
+        <thead>
+            <tr>
+                <th>Account Number</th>
+                <th>Account Type</th>
+                <th>Last Modified</th>
+                <th>Balance</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($accounts as $account) : ?>
+                <tr>
+                    <td><?= $account['account_number'] ?></td>
+                    <td><?= $account['account_type'] ?></td>
+                    <td><?= $account['modified'] ?></td>
+                    <td><?= number_format($account['balance'], 2) ?></td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
+<?php else : ?>
+    <p>No accounts found.</p>
+<?php endif ?>
 
+<a href="create_account.php">Create New Account</a>
 
-<?php
-function list_accounts($user_id) {
-    $db = getDB();
-    
-    // Prepare the SQL statement
-    $stmt = $db->prepare('SELECT account_number, account_type, modified, balance FROM Accounts WHERE user_id = ? LIMIT 5');
-    $stmt->execute([$user_id]);
-    
-    // Fetch the results
-    $accounts = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    // Output the results in a table
-    echo '<table>';
-    echo '<tr><th>Account Number</th><th>Account Type</th><th>Modified</th><th>Balance</th></tr>';
-    foreach ($accounts as $account) {
-        echo '<tr>';
-        echo '<td>' . htmlspecialchars($account['account_number']) . '</td>';
-        echo '<td>' . htmlspecialchars($account['account_type']) . '</td>';
-        echo '<td>' . htmlspecialchars($account['modified']) . '</td>';
-        echo '<td>' . htmlspecialchars($account['balance']) . '</td>';
-        echo '</tr>';
-    }
-    echo '</table>';
-}
-?>
-
+</body>
+</html>
 
 <?php
 require(__DIR__ . "/../../partials/flash.php");
